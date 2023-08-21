6acb276b667da0827451c58ba8b32c67aafd998c
JenniferEinstein <jennifereinstein@gmail.com>
2023-08-21 14:17:46 -0400

    The rest of the initial commit.

---
+++ src/App.js
@@ -0,0 +1,32 @@
+//DEPENDENCIES
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//STYLING
+import './App.css';
+
+//PAGES
+import Home from "./Pages/Home"
+import Index from "./Pages/Index"
+import Show from "./Pages/Show"
+import Edit from "./Pages/Edit"
+import New from "./Pages/New"
+import PageNotFound from "./Pages/PageNotFound"
+
+
+//COMPONENTS
+import NavBar from "./Components/NavBar"
+import Footer from "./Components/Footer"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
