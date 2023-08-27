0852e4c7d235bee94b1fcebfd074712f3e57765e
JenniferEinstein <jennifereinstein@gmail.com>
2023-08-26 22:45:28 -0400

    Worked on NavBar. Has logo and tabs and some styling.

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,39 +1,39 @@
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import "../common/NavBar.css";
+import logo from "../assets/TypeWriter Keys.png"
  
 export default function NavBar() {
-  console.log("This is the NavBar component.")
-  let navigate = useNavigate();
+  const navigate = useNavigate();
+
+
+  const handleTabClick = (tabName) => {
+    if (tabName === 'home') {
+      navigate('/');
+    } else if (tabName === 'entries') {
+      navigate('/entries');
+    } else if (tabName === 'newEntry') {
+      navigate('/entry/new');
+    } else if (tabName === 'about') {
+      // Handle your about route here
+    }
+  };
 
   return (
-    <nav>NavBar - tealish
-      
-      <button className='btn'  onClick = {() => {
-            navigate('/')
-      }}>
-        Home
-      </button>
+    <nav className='topnav'>
+      <div className="logo">
+        <img src={logo} alt="Logo" className="logo-img" />
+      </div>  
+   
+      <div className="nav-links">
+        <a onClick={() => handleTabClick('home')}>Home</a>
+        <a onClick={() => handleTabClick('entries')}>See All Entries</a>
+        <a onClick={() => handleTabClick('newEntry')}>Create a New Entry</a>
+        <a href="#about">About</a>
+      </div>
 
-
-
-      <button className='btn'  onClick = {() => {
-            console.log('The see all button was clicked');
-            navigate('/entries')
-      }}>
-        See All Entries
-      </button>
-
-
-
-      <button className='btn'  onClick = {() => {
-            console.log('The new button was clicked');
-            navigate('/entry/new')
-      }}>
-        Create a New Entry
-      </button>
+   
 
     </nav>
   )
 }
-
-//basically copied from lab-express-sql-seed-read
